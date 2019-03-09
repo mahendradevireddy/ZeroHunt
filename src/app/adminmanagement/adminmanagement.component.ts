11a5e2a997d87a95a7b52dc9d8d2eb694378bf62
Mahendra Devireddy <mahendra.drd@gmail.com>
2019-03-09 14:20:12 +0530

    some

---
+++ src/app/adminmanagement/adminmanagement.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-adminmanagement',
+  templateUrl: './adminmanagement.component.html',
+  styleUrls: ['./adminmanagement.component.css']
+})
+export class AdminmanagementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminmanagementComponent } from './adminmanagement/adminmanagement.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminmanagementComponent
   ],
   imports: [
     BrowserModule,
