677dde55ce699895dbae5277ff78bf7784710719
Mahendra Devireddy <mahendra.drd@gmail.com>
2019-03-13 22:37:25 +0530

    With routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminmanagementComponent } from './adminmanagement/adminmanagement.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'Admin', component:AdminmanagementComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
